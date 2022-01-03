69a00c1ea00e318ebb5a56af54f5538fb6a82018
akeda <akeda2@gmail.com>
2022-01-03 23:09:47 +0100

    fansettings defaults

--- fansettings.defaults.py
+++ fansettings.defaults.py
@@ -22,15 +22,8 @@ class FanSettings:
     fansett2 = [15, 30, 65, 30, 100]
     fansetts = [fansett0, fansett1, fansett2]
     
-    #def fan1(self):
-    #    print("fan1")
-    #    return self.fansett1
-    #def fan2(self):
-    #    print("fan2")
-    #    return self.fansett2
     def getsett(self, myfan: int):
         return self.fansetts[myfan]
     def howmany(self):
-        #print(self.fans, "fans defined!")
         return len(self.fansetts) - 1
     
