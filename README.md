e6b12a80b3307d921dd90b64493ebec19adb9c8c
akeda <akeda2@gmail.com>
2022-01-04 21:52:04 +0100

    Cleanup and README edit

--- README.md
+++ README.md
@@ -4,11 +4,21 @@ Tools for using the pico (rd2040) as a pwm fan controller
 ## Includes
 ```
 tempsend.py - Python-script to run as service
-pifanpwm.service - service
+pifanpwm.service - service to run on linux host.
 main.py - program to run on controller (rd2040 like the Raspberry Pi Pico)
+fan.py - fanlib
+settings.py - both tempsend.py and main.py will look for this,
+create one from settings.defaults.py and customise as you wish.
+
+Copy main.py, fan.py and settings.py to a Pico-board and connect pins to fan pwm.
 ```
 ## Usage:
-Copy to/Place symlink in /usr/local/bin:
+### Semi-auto:
+```
+sudo make_install.sh
+```
+### Or manually:
+Copy or symlink to /usr/local/bin:
 ```
 sudo cp tempsend.py /usr/local/bin
 OR:
@@ -17,6 +27,7 @@ sudo ln -s $(pwm)/tempsend.py /usr/local/bin/tempsend.py
 Copy service, enable it and run:
 ```
 sudo cp pifanpwm.service /etc/systemd/system/
+cd /etc/systemd/system
 sudo systemctl enable pifanpwm.service
 sudo systemctl start pifanpwm.service
 ```
@@ -37,8 +48,11 @@ Use GPU/CPU-data:
 ```
 python3 tempsend.py gpu
 python3 tempsend.py cpu
+python3 tempsend.py both
 ```
+The service will run both as default, which will send two values to the pico:
+ex: 100045 for fan1 and 200034 for fan2.
 The CPU-mode will use '/sys/class/thermal/thermal_zone3/temp' as default.
-Change this to what is best for your system.
+Change this in settings.py to what is best for your system.
 
 All this might change a lot!
--- main.py
+++ main.py
@@ -6,7 +6,7 @@ from fan import Fan
 from _thread import start_new_thread
 import gc
 from machine import Pin, Timer, PWM
-import legacy
+#import legacy
 from fansettings import FanSettings
 
 gc.enable()
@@ -21,35 +21,21 @@ print("Running on",board)
 howmany = fanconf.howmany()
 print(howmany, "fans")
 
-#for p in howmany:
-#    fansett
-
 # GPU-fan:
-#fan1sett = [15, 30, 55, 30, 100]
-fan1sett = fanconf.getsett(1)
-print(str(fan1sett))
-
 # CPU-fan:
-#fan2sett = [15, 30, 65, 30, 100]
-fan2sett = fanconf.getsett(2)
-print(str(fan2sett))
 
 # For regular RPi-pico
 if board == "pico":
-    #fan = PWM(Pin(12))
+    # Which pin to begin with (the first '0' is the dummy):
     pinbegin = 17
-    #fan1 = Fan(16, fan1sett)
-    #fan2 = Fan(15, fan2sett)
     
     led = PWM(Pin(25))
     led.freq(1000)
 
 # For tiny2040
 if board == "tiny":
-    #fan = PWM(Pin(7))
+    # Which pin to begin with (the first '0' is the dummy):
     pinbegin = 7
-    #fan1 = Fan(6, fan1sett)
-    #fan2 = Fan(5, fan2sett)
     
     red = PWM(Pin(18))
     red.freq(1000)
@@ -59,15 +45,15 @@ if board == "tiny":
     blue = PWM(Pin(20))
     blue.freq(1000)
 
+# Make some fans:
 i = 0
 fans = []
 for p in range(fanconf.howmany()+1):
-    print(str(p))
+    #print(str(p))
     fans.append(Fan(pinbegin-i, fanconf.getsett(p)))
     i+=1
-    print(fans)
+print(len(fans), "fan objects including dummy '0'")
 
-#fan.freq(25000)
 data = 66
 rawdata = 0
 counter = 64
@@ -77,7 +63,7 @@ def readser():
     while True:
         arawdata = sys.stdin.readline()
         rawdata = arawdata
-        #print("-----------------------Thread-rawdata:", str(rawdata))
+        
         if counter < 64:
             counter = 64
         
@@ -86,30 +72,22 @@ def readser():
             green.duty_u16(65535)
         else:
             led.duty_u16(65535)
-    
-    #try:
-    #    data = int(rawdata)
-    #except:
-    #    print("Exception:", str(rawdata))
-    #    pass
+
         gc.collect()
-    #utime.sleep(1)
 
 readserialThread = _thread.start_new_thread(readser, ())
 
 last = 66
 
 while True:
-#    readser()
     utime.sleep(0.2)
     try:
         data = int(rawdata)
     except:
         print("Currupt serial input?", data)
-        data = 100065
+        data = 65
         pass
-    
-    
+
     changed = last != data
     if changed:
         #print("Value changed")
@@ -133,9 +111,9 @@ while True:
         data = 80
     else:
         counter -= 1
+        
     # Did we just receive temperature?
     # And more than one fan?
-    
     if data > 99000 and changed:
         print(str(data))
         try:
@@ -143,45 +121,24 @@ while True:
             e = int(d[0])
             print("Fan", str(e))
             fans[e].setpwm(data - (e * 100000))
-            #fan2.setpwm(data - 200000)
+            #fans[int(str(data)[0])].setpwm(data - (e * 10000))
         except:
             print("Fail", str(data))
         data = None
-    #elif data > 99000 and changed:
-    #    print(str(data))
-    #    try:
-    #        fan1.setpwm(data - 100000)
-    #    except:
-    #        print("Fail 1")
-    #        pass
-    #    data = ''
+        
     elif data > 9000 and changed:
-        #legacy.setFanSpeed(temp2pwm(data - 10000))
         for u in fans:
             u.setpwm(data - 10000)
-            #fans[int(str(data)[0])].setpwm(data - (e * 10000))
-        #fan1.setpwm(fan1.temp22pwm(data - 10000))
-        #fan2.setpwm(fan2.temp22pwm(data - 10000))
         print("FAIL!")
-    elif data == 80:
+    elif data <= 100 and data > 0:
         # Or just pwm duty?
-        #legacy.setFanSpeed(data)
         for u in fans:
             u.setpwm(data)
-        #fan1.setpwm(data)
-        #fan2.setpwm(data)
-        #print("---Else!")
-    elif data == 66:
-        for u in fans:
-            u.setpwm(data)
-        #fan1.setpwm(66)
-        #fan2.setpwm(66)
-    
-    if board == "tiny":
-        blue.duty_u16(65535)
         
     if board == "tiny":
+        blue.duty_u16(65535)
         red.duty_u16(65535)
     else:
         led.duty_u16(600)
     gc.collect()
+    
